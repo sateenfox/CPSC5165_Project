52d8612be0692a178940202dca45aedc959874c0
Savannah Brown <brown_savannah@columbusstate.edu>
2018-05-08 12:34:41 +0000

    redid shop page

---
+++ shop.php
@@ -0,0 +1,113 @@
+<?php
+
+?>
+<html lang = "en">
+<head>
+	<meta charset = "utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel = "stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Raleway">
+	<link rel = "stylesheet" href = "styles/custom.css">
+	<title>Shop | TeAmo Tea</title>
+</head>
+
+<body>
+	<div class = "container-fluid justify-content-around">
+		<!--Navigation Bar-->
+		<div class = "row justify-content-around">
+			<nav class = "nav">
+				<a class = "nav-link" href = "index.php">Home</a>
+				<a class = "nav-link" href = "menu.php">Menu</a>
+				<a class = "nav-link" href = "shop.php">Shop</a>
+				<a class = "nav-link" href = "about.php">About</a>
+				<a class = "nav-link" href = "contact.php">Contact</a>
+				<a class = "nav-link" href = "form.php">Sign up</a>
+				<a class = "nav-link" href = "login.php">Login</a>
+				<a class = "nav-link" href = "search.php">Search</a>
+			</nav>
+		</div>
+		<div class = "row justify-content-center">
+			<h1>Shop</h1>
+		</div>
+        <div class = "row justify-content-center">
+            <h2>Rooibos</h2>
+        </div>
+        <div class = "row justify-content-around">
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f27605e4b01ed5bc41a716/55f27821e4b0355f89ace977/1441953826000/IMG_1129.JPG?format=750w">
+                <p>South African Rooibos</p>
+                <p>$3.99</p>
+            </div>
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f27605e4b01ed5bc41a716/55f279e8e4b08cb02e0bc86c/1442109744866/IMG_1130.JPG?format=750w">
+                <p>Citrus Rooibos</p>
+                <p>$3.99</p>
+            </div>
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f27605e4b01ed5bc41a716/55f27a6ce4b08cb02e0bc989/1441954412960/IMG_1134.JPG?format=750w">
+                <p>Red Island</p>
+                <p>$3.99</p>
+            </div>
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f27605e4b01ed5bc41a716/5682588d841abae3b7db3470/1451382926237/IMG_1386.JPG?format=750w">
+                <p>Wild Blueberry and Creme Rooibos</p>
+                <p>$3.99</p>
+            </div>
+        </div>
+        <div class = "row justify-content-center">
+        <h2>Green Teas</h2>
+        </div>
+        <div class = "row justify-content-around">
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f220d2e4b055df02a049b8/55f23e7fe4b07daab639032e/1441939072908/IMG_1101.JPG?format=750w">
+                <p>Dragonwell Green</p>
+                <p>$4.49</p>
+            </div>
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f220d2e4b055df02a049b8/55f25d66e4b06fa71e7f3d5c/1441946983264/IMG_1114.JPG?format=750w">
+                <p>Citrus Green</p>
+                <p>$4.49</p>
+            </div>
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f220d2e4b055df02a049b8/55f2426fe4b044c076d50f6a/1441940079454/IMG_1118.JPG?format=750w">
+                <p>Three Amigos</p>
+                <p>$4.49</p>
+            </div>
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f220d2e4b055df02a049b8/55f25ef8e4b0dbacd21e8651/1441947385277/IMG_1136.JPG?format=750w">
+                <p>Sencha</p>
+                <p>$4.49</p>
+            </div>
+        </div>
+        <div class = "row justify-content-center">
+            <h2>Herbal Teas</h2>
+        </div>
+        <div class = "row justify-content-around">
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f528e3e4b0e0c7bc127ddd/55f52e41e4b01e760d778050/1442131522054/IMG_1120.JPG?format=750w">
+                <p>Garden of Herbs</p>
+                <p>$3.99</p>
+            </div>
+            <div class = "col">
+                <img class = "tea-img" src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f528e3e4b0e0c7bc127ddd/55f52d50e4b0300e70c57e15/1442131281306/IMG_1121.JPG?format=750w">
+                <p>Very Berry Herbal Tea</p>
+                <p>$3.99</p>
+            </div>
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f528e3e4b0e0c7bc127ddd/55f52a9ee4b0df783b21b791/1442130590518/IMG_1125.JPG?format=750w">
+                <p>Herbal Orange Spice</p>
+                <p>$3.99</p>
+            </div>
+            <div class = "col">
+                <img class = "tea-img"  src = "https://static1.squarespace.com/static/55ee6a32e4b02e95293dc262/55f528e3e4b0e0c7bc127ddd/55f52bcde4b07856e00966af/1442130893950/IMG_1124.JPG?format=750w">
+                <p>Fall Fruit Lovers</p>
+                <p>$3.99</p>
+            </div>
+        </div>
+	</div>
+	<!--Bootstrap Scripts-->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
