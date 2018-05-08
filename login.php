8e5f2f4cf523c116191f750e1782484762a5a3d8
Savannah Brown <brown_savannah@columbusstate.edu>
2018-05-08 12:37:12 +0000

    fixed links

--- login.php
+++ login.php
@@ -44,6 +44,7 @@
 			<nav class = "nav">
 				<a class = "nav-link" href = "index.php">Home</a>
 				<a class = "nav-link" href = "menu.php">Menu</a>
+				<a class = "nav-link" href = "shop.php">Shop</a>
 				<a class = "nav-link" href = "contact.php">Contact</a>
 				<a class = "nav-link" href = "form.php">Sign Up</a>
 				<a class = "nav-link" href = "login.php">Login</a>
